35c8e6cfddc0da63c90328a281a1b71a6057eeb2
surya2406 <95280700+surya2406@users.noreply.github.com>
2025-04-22 14:44:26 +0530

    Update cache.py

--- app/cache.py
+++ app/cache.py
@@ -1,14 +1,27 @@
+# from fastapi_cache import FastAPICache
+# from fastapi_cache.backends.redis import RedisBackend
+# import redis.asyncio as redis
+
+
+# redis_client=redis.Redis(
+#         host="redis",
+#         port=6379,
+#         db=2,
+#         decode_responses=True
+#     )
+# async def init_cache():
+    
+#     FastAPICache.init(RedisBackend(redis_client),prefix="fastapi-cache")
+
+#for cloud
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 import redis.asyncio as redis
+import os
 
+REDIS_URL = os.getenv("REDIS_URL") 
 
-redis_client=redis.Redis(
-        host="redis",
-        port=6379,
-        db=2,
-        decode_responses=True
-    )
+redis_client = redis.from_url(REDIS_URL, decode_responses=True)
+
 async def init_cache():
-    
-    FastAPICache.init(RedisBackend(redis_client),prefix="fastapi-cache")
+    FastAPICache.init(RedisBackend(redis_client), prefix="fastapi-cache")
